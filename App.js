96fb5f04f8c38683d56828ecac915e40799b17da
Jazziel Bello <83053212+Jaxbel@users.noreply.github.com>
2024-05-07 12:01:53 -0700

    app

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Main from './src/components/Main';
+
+export default function App() {
+  return (
+      <Main />
+  );
+}
